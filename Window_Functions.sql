ab018b12231ac0c54f9c43fc39203a405cd1bf23
Sudheer_bille <148851067+sudheerbille@users.noreply.github.com>
2026-08-30 23:36:13 +0530

    Update Window_Functions.sql

--- Window_Functions.sql
+++ Window_Functions.sql
@@ -3,27 +3,36 @@ Sample Data: employees Table
 CREATE TABLE employees (
     emp_id INT,
     emp_name VARCHAR(50),
-    department VARCHAR(50),
+    dept VARCHAR(50),
     salary INT
 );
 
-INSERT INTO employees VALUES
-(101, 'Anil',   'IT', 90000),  -- Highest in IT (Unique)
-(102, 'Bhavna', 'IT', 80000),  -- Tie 1 for 2nd place in IT
-(103, 'Charan', 'IT', 80000),  -- Tie 2 for 2nd place in IT
-(104, 'Divya',  'IT', 70000),  -- Follows the tie
-(105, 'Eswar',  'HR', 60000),  -- Top in HR (Unique)
-(106, 'Farooq', 'HR', 50000),  -- Tie 1 for 2nd place in HR
-(107, 'Gita',   'HR', 50000),  -- Tie 2 for 2nd place in HR
-(108, 'Hari',   'HR', 50000),  -- Tie 3 for 2nd place in HR
-(109, 'Indu',   'HR', 40000);  -- Follows a 3-way tie
+INSERT INTO EMPLOYEES (EMP_ID, NAME, SALARY, DEPT) VALUES
+(101, 'ANIL', 90000, 'IT'),
+(110, 'SUDHEER', 90000, 'IT'),
+(106, 'INDU', 85000, 'HR'),
+(102, 'BHAVANA', 80000, 'IT'),
+(107, 'CHARAN', 80000, 'IT'),
+(108, 'DIVYA', 70000, 'IT'),
+(103, 'ESWAR', 50000, 'HR'),
+(104, 'FAROOQ', 50000, 'HR'),
+(105, 'HARI', 50000, 'HR'),
+(109, 'GITA', 40000, 'HR');
 
 -- SQL Query to Demonstrate the Differences
+
+-- Without Partition by 
 SELECT 
-    department,
-    emp_name,
-    salary,
-    ROW_NUMBER() OVER (PARTITION BY department ORDER BY salary DESC) AS row_num,
-    RANK()       OVER (PARTITION BY department ORDER BY salary DESC) AS rnk,
-    DENSE_RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS dense_rnk
+    *,
+    ROW_NUMBER() OVER ( ORDER BY salary DESC) AS row_num,
+    RANK()       OVER ( ORDER BY salary DESC) AS rnk,
+    DENSE_RANK() OVER ( ORDER BY salary DESC) AS dense_rnk
+FROM employees;
+
+--With Partition by 
+SELECT 
+    *,
+    ROW_NUMBER() OVER (PARTITION BY dept ORDER BY salary DESC) AS row_num,
+    RANK()       OVER (PARTITION BY dept ORDER BY salary DESC) AS rnk,
+    DENSE_RANK() OVER (PARTITION BY dept ORDER BY salary DESC) AS dense_rnk
 FROM employees;
